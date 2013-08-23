ea53377746dd3477d1957fae0dce80806d6414f0
Marisela <pscorreo@gmail.com>
2013-08-23 10:32:52 -0500

    clean up for final push to github

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
 
-
-	// $('.fadein img:gt(0)').hide();
-
 	// When you click on a thumbnail,
 	// 1. remove .active from all thumbnails
-	// 2. Add .active to the clicke thumbnail
+	// 2. Add .active to the clicked thumbnail
 	// 3. Fade out current image
 	// 3. Fade in the clicked image
 	
@@ -17,17 +14,8 @@ $(document).ready(function(){
 
 		$('#panel img').hide();
 		$('#panel img[src="' + targetsrc + '"]').fadeIn(1000);
-});
-
-
-
-     
-     // setInterval(function(){
-     //  $('.fadein :first-child').fadeOut()
-     //     .next('img').fadeIn()
-     //     .end().appendTo('.fadein');
-     // }, 3000);
-
 	});
+
+});
 
 	
